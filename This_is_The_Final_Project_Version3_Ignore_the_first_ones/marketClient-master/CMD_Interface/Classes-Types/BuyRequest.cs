836f0be295da8aa6213917966174a6e0b8ccd6cf
Ygrno <yakirgre@post.bgu.ac.il>
2017-03-29 16:01:19 +0300

    milestone_<#Finish3>

---
+++ This_is_The_Final_Project_Version3_Ignore_the_first_ones/marketClient-master/CMD_Interface/Classes-Types/BuyRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlgoTrading
+{
+    class BuyRequest
+    {
+        public int price;
+        public int amount;
+        public int commodity;
+        public String type="buy";
+        public BuyRequest(int price,int amount,int commodity)
+        {
+            this.price = price;
+            this.amount = amount;
+            this.commodity = commodity;                  
+        }
+
+    }
+}
---
+++ This_is_The_Final_Project_Version3_Ignore_the_first_ones/marketClient-master/CMD_Interface/Classes-Types/QueryMarket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgoTrading
+{
+    class QueryMarket
+    {
+        public String type = "queryMarket";
+        public int commodity;
+        public QueryMarket(int commodity)
+        {
+            this.commodity = commodity;
+        }
+    }
+}
