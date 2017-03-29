3088eeeb526c01519d9899a57e5cd1b1c3d5a3b7
Ygrno <yakirgre@post.bgu.ac.il>
2017-03-29 15:29:12 +0300

    milestone_<#Finish2>

---
+++ This_is_The_Final_Project_Version2_Ignore_the_first_one/marketClient-master/CMD_Interface/Classes-Types/QueryBuySell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AlgoTrading
+{
+    class QueryBuySell
+    {
+        public String type = "queryBuySell";
+        public int id;       
+
+        public QueryBuySell(int id)
+        {
+            this.id = id;
+        }
+    }
+}
